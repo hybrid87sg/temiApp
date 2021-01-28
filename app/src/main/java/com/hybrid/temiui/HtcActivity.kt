80c0ef06311f26d77bfa8617efb3e50def7846ca
hybrid87sg <75988430+hybrid87sg@users.noreply.github.com>
2021-01-29 00:08:20 +0800

    TemiUI 290121 0008

--- app/src/main/java/com/hybrid/temiui/HtcActivity.kt
+++ app/src/main/java/com/hybrid/temiui/HtcActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.airbnb.lottie.LottieDrawable
 import com.hybrid.temiui.databinding.ActivityHtcBinding
 
 class HtcActivity : AppCompatActivity(R.layout.activity_htc) {
